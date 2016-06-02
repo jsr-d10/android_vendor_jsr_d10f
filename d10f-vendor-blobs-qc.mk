5936316f3e15ae89d9ebedd8ab1f156d939e230e
remittor <remittor@gmail.com>
2016-06-02 10:24:26 +0300

    Fix prebuilts for full ROM build

---
+++ d10f-vendor-blobs-qc.mk
@@ -0,0 +1,58 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Camera
+
+#PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/lib/libmmcamera_t4k37ab.so:system/vendor/lib/libmmcamera_t4k37ab.so
+#PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/lib/libmmcamera_f4k37ab_qtech_t4k37_eeprom.so:system/vendor/lib/libmmcamera_f4k37ab_qtech_t4k37_eeprom.so
+#PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/lib/libchromatix_t4k37ab_common.so:system/vendor/lib/libchromatix_t4k37ab_common.so
+#PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/lib/libchromatix_t4k37ab_default_video.so:system/vendor/lib/libchromatix_t4k37ab_default_video.so
+#PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/lib/libchromatix_t4k37ab_preview.so:system/vendor/lib/libchromatix_t4k37ab_preview.so
+#PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/lib/libchromatix_t4k37ab_snapshot.so:system/vendor/lib/libchromatix_t4k37ab_snapshot.so
+
+# Keystore
+
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/firmware/keymaster/keymaster.b00:system/vendor/firmware/keymaster/keymaster.b00
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/firmware/keymaster/keymaster.b01:system/vendor/firmware/keymaster/keymaster.b01
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/firmware/keymaster/keymaster.b02:system/vendor/firmware/keymaster/keymaster.b02
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/firmware/keymaster/keymaster.b03:system/vendor/firmware/keymaster/keymaster.b03
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/firmware/keymaster/keymaster.mdt:system/vendor/firmware/keymaster/keymaster.mdt
+
+# ALPS Azimuth Calculation Module
+
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/vendor/lib/libhscdtd.so:system/vendor/lib/libhscdtd.so
+
+# Venus (media) firmware
+
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/venus.b00:system/etc/firmware/venus.b00
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/venus.b01:system/etc/firmware/venus.b01
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/venus.b02:system/etc/firmware/venus.b02
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/venus.b03:system/etc/firmware/venus.b03
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/venus.b04:system/etc/firmware/venus.b04
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/venus.mbn:system/etc/firmware/venus.mbn
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/venus.mdt:system/etc/firmware/venus.mdt
+
+# Wifi
+
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/wcnss.b00:system/etc/firmware/wcnss.b00
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/wcnss.b01:system/etc/firmware/wcnss.b01
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/wcnss.b02:system/etc/firmware/wcnss.b02
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/wcnss.b04:system/etc/firmware/wcnss.b04
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/wcnss.b06:system/etc/firmware/wcnss.b06
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/wcnss.b07:system/etc/firmware/wcnss.b07
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/wcnss.b08:system/etc/firmware/wcnss.b08
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/wcnss.b09:system/etc/firmware/wcnss.b09
+PRODUCT_COPY_FILES += vendor/jsr/d10f/proprietary/etc/firmware/wcnss.mdt:system/etc/firmware/wcnss.mdt
+
